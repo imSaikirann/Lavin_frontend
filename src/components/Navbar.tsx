f94c03731e5d36ca2bac7744c0a80af403733816
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-10-07 21:59:00 +0530

    new nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,11 @@ import SeacrhLogo from '../assets/search_icon.png'
 import Profile from '../assets/profile_icon.png'
 import Cart from '../assets/cart_icon.png'
 import Menu from '../assets/menu_icon.png'
+import Back from '../assets/dropdown_icon.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [visble,setVisble] = useState(false)
     return (
         <div className='flex items-center justify-between h-[75px] md:h-[90px] lg:h-[100px] font-medium font-raleway'>
             <img src={Logo} className='w-24 md:w-28 lg:w-32'></img>
@@ -26,7 +29,23 @@ const Navbar = () => {
                     <img src={Cart} className='w-5'></img>
                     <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black text-white rounded-full aspect-square text-[8px] leading-4'>2</p>
                 </Link>
-                <img src={Menu} className='flex sm:hidden w-5 cursor-pointer'></img>
+                <img onClick={()=>setVisble(true)} src={Menu} className='flex sm:hidden w-5 cursor-pointer'></img>
+            </div>
+
+            <div className={`absolute top-0 right-0 bottom-0 bg-white transition-all ${visble ? "w-full":"w-0"}`}>
+           <div onClick={()=>setVisble(!visble)} className='flex p-6 flex-row gap-4 items-center cursor-pointer'>
+           <img src={Back} className=' h-4 rotate-180'></img>
+           Back
+           </div>
+                <div className='flex flex-col '>
+                <NavLink to="/" className=" py-2 pl-6 border-b ">Home</NavLink>
+                <NavLink to="/collections" className=" py-2 pl-6 border-b ">Collections</NavLink>
+                <NavLink to="/collections" className=" py-2 pl-6 border-b ">About us</NavLink>
+
+                </div>
+            </div>
+            <div>
+                
             </div>
         </div>
     )
