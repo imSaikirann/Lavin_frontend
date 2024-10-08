3fcb5b172aeb03ee10262b5b690837b2f8893404
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-10-08 16:23:17 +0530

    new nav

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -2,35 +2,53 @@ import Book from '../assets/lavin3.jpg';
 
 const Collection = () => {
   return (
-    <div className="my-10 px-4 md:px-8">
+    <div className="my-10 px-4 md:px-8 max-w-screen-xl mx-auto">
       {/* Title Section */}
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center mb-8">
         <h1 className="text-xl md:text-2xl lg:text-4xl font-medium font-poppins text-gray-800">
           Latest Products
         </h1>
       </div>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 my-8">
-        <div className="overflow-hidden p-2 lg:p-4   bg-white transition-shadow duration-300 ease-in-out cursor-pointer">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+        {/* Product Card */}
+        <div className="p-2 lg:p-4 h-64 md:h-56 lg:h-72 bg-white  transition-shadow duration-300 ease-in-out cursor-pointer rounded-lg">
           {/* Product Image */}
-          <div className="overflow-hidden ">
+          <div className="overflow-hidden rounded-lg">
             <img
               src={Book}
               alt="Lavin Notebook"
-              className="w-full h-full object-cover  transform hover:scale-105 transition-transform duration-300 ease-in-out"
+              className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-300 ease-in-out"
             />
           </div>
+
           {/* Product Info */}
           <div className="flex items-center justify-between pt-3 pb-1 p-1">
             <p className="text-md font-medium font-raleway text-gray-700">Lavin Notebook</p>
             <p className="text-md font-medium font-raleway text-main">$8</p>
           </div>
-          {/* View or Buy Button */}
-         
         </div>
 
         {/* Repeat for more products */}
+        <div className="p-2 lg:p-4 h-64  md:h-56 lg:h-72 bg-white  transition-shadow duration-300 ease-in-out cursor-pointer rounded-lg">
+          {/* Product Image */}
+          <div className="overflow-hidden rounded-lg">
+            <img
+              src={Book}
+              alt="Lavin Notebook"
+              className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-300 ease-in-out"
+            />
+          </div>
+
+          {/* Product Info */}
+          <div className="flex items-center justify-between pt-3 pb-1 p-1">
+            <p className="text-md font-medium font-raleway text-gray-700">Lavin Notebook</p>
+            <p className="text-md font-medium font-raleway text-main">$8</p>
+          </div>
+        </div>
+
+        {/* Add more product cards as needed */}
       </div>
     </div>
   );
