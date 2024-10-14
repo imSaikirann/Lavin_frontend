2d991e3b698f93f1c4168d36ae3879d079435210
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-10-14 22:33:45 +0530

    product

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Book from '../assets/lavin3.jpg';
 
 const Collection = () => {
@@ -11,27 +12,29 @@ const Collection = () => {
 </div>
 
 
-      {/* Products Grid */}
+    
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {/* Product Card */}
-        <div className="p-2 lg:p-4 h-64 md:h-56 lg:h-72 bg-white  transition-shadow duration-300 ease-in-out cursor-pointer rounded-lg">
-          {/* Product Image */}
-          <div className="overflow-hidden ">
-            <img
-              src={Book}
-              alt="Lavin Notebook"
-              className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-300 ease-in-out"
-            />
-          </div>
+    
+      <Link to="/product">
+      <div className="p-2 lg:p-4 h-64 md:h-56 lg:h-72 bg-white  transition-shadow duration-300 ease-in-out cursor-pointer rounded-lg">
+     
+     <div className="overflow-hidden ">
+       <img
+         src={Book}
+         alt="Lavin Notebook"
+         className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-300 ease-in-out"
+       />
+     </div>
 
-          {/* Product Info */}
-          <div className="flex items-center justify-between pt-3 pb-1 p-1">
-            <p className="text-md font-medium font-raleway text-gray-700">Lavin Notebook</p>
-            <p className="text-md font-medium font-raleway text-main">$8</p>
-          </div>
-        </div>
+     {/* Product Info */}
+     <div className="flex items-center justify-between pt-3 pb-1 p-1">
+       <p className="text-md font-medium font-raleway text-gray-700">Lavin Notebook</p>
+       <p className="text-md font-medium font-raleway text-main">$8</p>
+     </div>
+   </div>
+      </Link>
 
-        {/* Repeat for more products */}
+       
         <div className="p-2 lg:p-4 h-64  md:h-56 lg:h-72 bg-white  transition-shadow duration-300 ease-in-out cursor-pointer rounded-lg">
           {/* Product Image */}
           <div className="overflow-hidden ">
@@ -49,7 +52,7 @@ const Collection = () => {
           </div>
         </div>
 
-        {/* Add more product cards as needed */}
+      
       </div>
     </div>
   );
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,95 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Products } from "../types/types";
 
+const Product: React.FC = () => {
+    const [products, setProducts] = useState<Products[]>([]);
+    const [selectedProductIndex, setSelectedProductIndex] = useState<number>(0);
+    const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
+    
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const s3Url = import.meta.env.VITE_S3_URL; 
 
-const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+    const fetchData = async () => {
+        try {
+            const response = await axios.get<{ data: Products[]}>(`${apiUrl}/api/v1/products/getProducts`);
+            setProducts(response.data.data);
+            console.log(response.data.data); // Log fetched products
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
-export default Product
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const selectedProduct = products[selectedProductIndex] || null;
+
+    return (
+        <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+            <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+                <div className="flex-1 flex flex-row gap-3 sm:flex-row">
+                    <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll gap-3 justify-between sm:justify-normal sm:w-[18.7%] w-full">
+                        {products.map((product, productIndex) => (
+                            <div key={product.id}>
+                                {product.images.map((image, imageIndex) => (
+                                    <img
+                                        onClick={() => {
+                                            setSelectedProductIndex(productIndex); 
+                                            setSelectedImageIndex(imageIndex); 
+                                        }} 
+                                        key={imageIndex}
+                                        src={`${s3Url}/${image}`} 
+                                        alt={`Product image ${imageIndex + 1}`}
+                                        className="w-[20%] sm:w-full  sm:mb-3 flex-shrink-0 cursor-pointer"
+                                    />
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                    
+                    {/* Display selected product image */}
+                    <div className="w-full sm:w-[80%]">
+                        {selectedProduct && selectedProduct.images.length > 0 && (
+                            <img
+                                src={`${s3Url}/${selectedProduct.images[selectedImageIndex]}`} 
+                                alt="Selected Product"
+                                className="w-full h-auto"
+                            />
+                        )}
+                    </div>
+                </div>
+
+                {/* Display selected product name and price */}
+                <div className="flex-1">
+                    {selectedProduct ? (
+                        <div>
+                          <h1 className="font-medium text-2xl mt-2">{selectedProduct.productName}</h1>
+                        
+                          {selectedProduct.variants.length > 0 ? (
+                              <p className="mt-3 font-semibold text-2xl"> ₹{selectedProduct.variants[selectedImageIndex].price} </p>
+                             
+                          ) : (
+                              <p>No variants available</p>
+                          )}
+                           <p className="mt-5 text-gray-600 md:w-4/5"> {selectedProduct.productDescription} </p>
+                           {selectedProduct.variants.length > 0 ? (
+                              <p className="mt-3 font-semibold text-2xl"> {selectedProduct.variants[selectedImageIndex].color} </p>
+                             
+                          ) : (
+                              <p>No variants available</p>
+                          )}
+
+                          <button className="bg-black text-white px-5 py-2 mt-5 cursor-pointer">Add to Cart</button>
+                        </div>
+                    ) : (
+                        <p>No product selected</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
