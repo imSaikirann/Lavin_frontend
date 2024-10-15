fdadcac54aee2ddf24a1a96988a1e3b9beec07ea
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-10-15 20:07:16 +0530

    product redux

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-      <footer className="bg-white text-gray-800 ">
-        <div className="container mx-auto px-4">
+      <footer className="bg-white text-gray-800 py-8">
+        <div className="container mx-auto ">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             {/* About Section */}
             <div>
