d5cdf1f96b27c9831162d2a9e29fe55b2d88636a
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-10-20 19:54:37 +0530

    data

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Signin = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-t from-orange-400 to-orange-200">
-      <div className="bg-white p-6 sm:p-8 rounded-lg w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl">
+    <div className="min-h-screen flex items-center justify-center ">
+      <div className="bg-white p-6 sm:p-8 rounded-lg w-full shadow-lg max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl">
         <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-center mb-4 sm:mb-6">
           Sign In to Your Account
         </h2>
@@ -13,7 +13,7 @@ const Signin = () => {
             <label className="block text-sm font-medium text-gray-700">Email</label>
             <input
               type="email"
-              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-orange-300 focus:border-orange-300"
               placeholder="you@example.com"
             />
           </div>
@@ -23,7 +23,7 @@ const Signin = () => {
             <label className="block text-sm font-medium text-gray-700">Password</label>
             <input
               type="password"
-              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-orange-300 focus:border-orange-300"
               placeholder="********"
             />
           </div>
@@ -32,7 +32,7 @@ const Signin = () => {
           <div>
             <button
               type="submit"
-              className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-black text-white font-medium rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-main text-white font-medium rounded-md shadow hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Log In
             </button>
@@ -41,7 +41,7 @@ const Signin = () => {
 
         <p className="mt-3 sm:mt-4 text-center text-xs sm:text-sm text-gray-600">
           Don't have an account?{' '}
-          <a href="/signup" className="text-blue-600 hover:underline">
+          <a href="/signup" className="text-black font-bold hover:underline">
             Sign Up
           </a>
         </p>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-t from-orange-400 to-orange-200">
-      <div className="bg-white p-6 sm:p-8 rounded-lg w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl">
+    <div className="min-h-screen flex items-center justify-center ">
+      <div className="bg-white p-6 sm:p-8 rounded-lg w-full shadow-lg max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl font-poppins">
         <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-center mb-4 sm:mb-6">
           Create an Account
         </h2>
@@ -13,7 +13,7 @@ const Signup = () => {
             <label className="block text-sm font-medium text-gray-700">Full Name</label>
             <input
               type="text"
-              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-orange-300 focus:border-orange-300"
               placeholder="John Doe"
             />
           </div>
@@ -23,7 +23,7 @@ const Signup = () => {
             <label className="block text-sm font-medium text-gray-700">Email</label>
             <input
               type="email"
-              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-orange-300 focus:border-orange-300"
               placeholder="you@example.com"
             />
           </div>
@@ -33,7 +33,7 @@ const Signup = () => {
             <label className="block text-sm font-medium text-gray-700">Password</label>
             <input
               type="password"
-              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              className="mt-1 block w-full px-3 py-2 sm:px-4 sm:py-3 border border-gray-300 rounded-md shadow-sm focus:ring-orange-300 focus:border-orange-300"
               placeholder="********"
             />
           </div>
@@ -42,7 +42,7 @@ const Signup = () => {
           <div>
             <button
               type="submit"
-              className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-black text-white font-medium rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-main text-white font-medium rounded-md shadow hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Sign Up
             </button>
@@ -51,7 +51,7 @@ const Signup = () => {
 
         <p className="mt-3 sm:mt-4 text-center text-xs sm:text-sm text-gray-600">
           Already have an account?{' '}
-          <a href="/signin" className="text-blue-600 hover:underline">
+          <a href="/signin" className="text-black font-bold hover:underline">
             Sign In
           </a>
         </p>
